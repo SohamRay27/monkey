08bc6da14065999ce39588e5b12413e443922dd1
SohamRay27 <72454451+SohamRay27@users.noreply.github.com>
2020-10-17 20:01:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,124 @@
+
+var monkey , monkey_running
+var banana ,bananaImage, obstacle, obstacleImage
+var FoodGroup, obstaclesGroup,ground,food
+var score
+var survivalTime=0
+
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+  bananaImage = loadImage("banana.png");
+  obstaceImage = loadImage("obstacle.png");
+ 
+}
+
+
+
+function setup() {
+   createCanvas(600,600)
+//creating monkey
+monkey=createSprite(80,315,20,20);
+monkey.addAnimation( "moving", monkey_running);
+monkey.scale=0.1
+
+ground = createSprite(400,350,900,10);
+ground.velocityx=-4;
+ground.x=ground. width/2;
+console.log (ground.x)
+  
+  
+   FoodGroup = createGroup();
+   obstaclesGroup = createGroup(); 
+  
+}
+
+
+function draw() {
+background("white")
+  
+  
+  if(keyDown("space")){
+     monkey.velocityY=-5
+     }
+  
+  
+  if(ground.y>400){
+     ground.y=300
+     }
+  
+  
+  
+   monkey.velocityY = monkey.velocityY+0.8
+  
+  
+  monkey.collide(ground)
+
+ 
+  spawnFood()
+  spawnObstacles()
+  
+  
+  stroke( "white");
+textSize(20);
+fill("white");
+text("Score:"+ score, 500, 50);
+
+
+stroke( "black");
+textSize(20);
+fill("black");
+survivalTime=Math. ceil(frameCount/frameRate())
+text("Survival Time: "+ survivalTime, 100, 50);
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  drawSprites()
+}
+
+function spawnFood() {
+//write code here to spawn the Food
+if (frameCount % 80 === 0){
+banana = createSprite(600, 250, 40, 10);
+banana. y = random(120,200);
+banana. velocityX = -5;
+
+//assign lifetime to the variable
+banana. lifetime = 300;
+monkey_depth = banana. depth + 1;
+//add image of banana
+
+banana. addImage(bananaImage);
+banana. scale=0.05;
+
+//add each banana to the group
+FoodGroup.add(banana);
+}
+ }
+
+function spawnObstacles() {
+if (frameCount % 300 === 0) {
+obstacle = createSprite(800, 320, 10,40);
+obstacle. velocityX = -6;
+
+//add image to the obstacle
+obstacle. addImage(obstaceImage);
+obstacle. scale=0.15;
+//lifetime to the obstacle
+obstacle. lifetime = 300;
+//add each obstacle to the group
+obstaclesGroup.add(obstacle);
+}
+  }
+
+
+
